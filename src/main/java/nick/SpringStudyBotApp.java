f20fa1f65bd59a1c1073dd1730ef765a1950e55d
Nick <nikitaverhovod@gmail.com>
2023-01-16 18:49:15 +0300

    Bot init

---
+++ src/main/java/nick/SpringStudyBotApp.java
@@ -0,0 +1,11 @@
+package nick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudyBotApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStudyBotApp.class, args);
+    }
+}
---
+++ src/main/java/nick/config/BotInitializer.java
@@ -0,0 +1,32 @@
+package nick.config;
+
+import nick.services.BotService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Component
+public class BotInitializer {
+
+    private final BotService bot;
+
+    @Autowired
+    public BotInitializer(BotService bot) {
+        this.bot = bot;
+    }
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(bot);
+        } catch (TelegramApiException e){
+
+        }
+    }
+
+}
